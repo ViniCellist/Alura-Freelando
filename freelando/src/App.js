0f33adf5eddb5e5cf6dca281583b108ebbc5b25c
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-09-27 11:30:39 -0300

    fat states of brazil

--- freelando/src/App.js
+++ freelando/src/App.js
@@ -2,9 +2,40 @@ import { Botao } from "./components/Botao/Botao";
 import { CampoTexto } from "./components/CampoDeTexto/CampoDeTexto";
 import { Card } from "./components/Card/Card";
 import { Estilos } from "./components/EstilosGlobais/Estilos";
+import { ListaSuspensa } from "./components/ListaSuspensa/ListaSuspensa";
 import { ProvedorTema } from "./components/ProvedorTema/ProvedorTema";
 import { Tipografia } from "./components/Tipografia/Tipografia";
 import {Col, Container, Row} from "react-grid-system";
+
+const estadosBrasileiros = [
+  { "text": "Acre", "value": "AC" },
+  { "text": "Alagoas", "value": "AL" },
+  { "text": "Amapá", "value": "AP" },
+  { "text": "Amazonas", "value": "AM" },
+  { "text": "Bahia", "value": "BA" },
+  { "text": "Ceará", "value": "CE" },
+  { "text": "Distrito Federal", "value": "DF" },
+  { "text": "Espírito Santo", "value": "ES" },
+  { "text": "Goiás", "value": "GO" },
+  { "text": "Maranhão", "value": "MA" },
+  { "text": "Mato Grosso", "value": "MT" },
+  { "text": "Mato Grosso do Sul", "value": "MS" },
+  { "text": "Minas Gerais", "value": "MG" },
+  { "text": "Pará", "value": "PA" },
+  { "text": "Paraíba", "value": "PB" },
+  { "text": "Paraná", "value": "PR" },
+  { "text": "Pernambuco", "value": "PE" },
+  { "text": "Piauí", "value": "PI" },
+  { "text": "Rio de Janeiro", "value": "RJ" },
+  { "text": "Rio Grande do Norte", "value": "RN" },
+  { "text": "Rio Grande do Sul", "value": "RS" },
+  { "text": "Rondônia", "value": "RO" },
+  { "text": "Roraima", "value": "RR" },
+  { "text": "Santa Catarina", "value": "SC" },
+  { "text": "São Paulo", "value": "SP" },
+  { "text": "Sergipe", "value": "SE" },
+  { "text": "Tocantins", "value": "TO" }
+]
 
 function App() {
   return (
@@ -25,7 +56,7 @@ function App() {
               </Row>
               <Row>
                 <Col lg={4} md={4} sm={4}>
-                  <CampoTexto titulo="Estado" /> 
+                  <ListaSuspensa titulo="Estado" opcoes={estadosBrasileiros} />
                 </Col>
               </Row>
               <Row>
