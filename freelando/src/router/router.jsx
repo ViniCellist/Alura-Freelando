46478e5773caae66dd1523e46e9207609e3c63c3
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-09-30 11:14:45 -0300

    feat router

---
+++ freelando/src/router/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import PaginaInicial from "../pages/PaginaInicial";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <PaginaInicial />,
+        children: [],
+    },
+]);
