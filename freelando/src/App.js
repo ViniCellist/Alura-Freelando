f4aa0bb3b15b73209bdb3c406446ca4666c37ea4
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-10-01 12:09:41 -0300

    Update App.js

--- freelando/src/App.js
+++ freelando/src/App.js
@@ -1,8 +1,15 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./router/router"
+import { ProvedorTema } from "./components/ProvedorTema/ProvedorTema"
+import { Estilos } from "./components/EstilosGlobais/Estilos"
 
 function App () {
-  return(<RouterProvider router={router} />)
+  return(
+    <ProvedorTema>
+      <Estilos />
+      <RouterProvider router={router} />
+    </ProvedorTema>
+  )
 }
 
 export default App
