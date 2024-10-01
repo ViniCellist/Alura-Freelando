287632cbb80732a43e46a8893e9f7bdbe05c92ae
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-10-01 12:08:35 -0300

    criando rota para as próximas páginas

--- freelando/src/router/router.jsx
+++ freelando/src/router/router.jsx
@@ -1,10 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
 import PaginaInicial from "../pages/PaginaInicial";
+import SelecaoCliente from "../pages/cadastro/SelecaoCliente";
+import LayoutBaseCadastro from "../pages/cadastro/LayoutBaseCadastro";
+import LayoutBase from "../pages/LayoutBase";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <PaginaInicial />,
-        children: [],
+        element: <LayoutBase />,
+        children: [
+            {
+                path:'cadastro',
+                element: <LayoutBaseCadastro />,
+                children: [
+                    {
+                        path: '',
+                        element: <SelecaoCliente />
+                    },
+                    {
+                        path: 'cliente',
+                        element: <h1>Interesses</h1>
+                    },
+                    {
+                        path: 'dados-pessoais',
+                        element: <h1>Dados Pessoais</h1>
+                    },
+                    {
+                        path: 'concluido',
+                        element: <h1>Concluido</h1>
+                    }
+                ]
+            }
+        ],
     },
 ]);
