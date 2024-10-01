297eef470b87fa0658a6c4743bf7e4d40ab80c3e
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-10-01 12:09:35 -0300

    Update Tipografia.jsx

--- freelando/src/components/Tipografia/Tipografia.jsx
+++ freelando/src/components/Tipografia/Tipografia.jsx
@@ -46,7 +46,7 @@ const estilos = {
         font-size: 16px;
         line-height: 20px;
     `,
-    h1: `
+    legenda: `
         font-weight: 400;
         font-size: 14px;
         line-height: 17px;
